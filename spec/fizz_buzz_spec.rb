0d426bd188856d0117a7c4d86981db189fd5f608
Noel Ryan <noelgerard.ryan@gmail.com>
2019-10-31 16:37:20 +0100

    adding test to return buzz if number is divisable by 5

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -8,4 +8,8 @@ describe 'fizzbuzz' do
     it 'returns Fizz! if number is divisable by 3' do
         expect(fizzbuzz(3)).to eq 'Fizz!'
     end
+
+    it 'returns Buzz! if number is divisable by 5' do
+        expect(fizzbuzz(5)).to eq 'Buzz!'
+    end
 end

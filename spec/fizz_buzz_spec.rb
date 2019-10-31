ce5a6828e3014ac7aba19c02baa23c3d5b8229ee
Noel Ryan <noelgerard.ryan@gmail.com>
2019-10-31 16:32:41 +0100

    Adding second test to return fizz when a number is divisable by 3

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -4,4 +4,8 @@ describe 'fizzbuzz' do
     it 'returns 1 if number is 1' do
         expect(fizzbuzz(1)).to eq 1
     end
+
+    it 'returns Fizz! if number is divisable by 3' do
+        expect(fizzbuzz(3)).to eq 'Fizz!'
+    end
 end
